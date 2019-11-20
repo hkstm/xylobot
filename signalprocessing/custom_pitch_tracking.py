d7173431a307bc1991a1ec35328a06ad42d3506c
Kailhan <takowski@outlook.com>
2019-11-20 15:23:48 +0100

    Added own fft calc

--- signalprocessing/custom_pitch_tracking.py
+++ signalprocessing/custom_pitch_tracking.py
@@ -39,6 +39,35 @@ def time_axis(total_segments, t_max):
     """Returns a list of times which correspond to the bins in the returned data from stft()"""
     return np.arange(total_segments) / np.float32(total_segments) * t_max
 
+#https://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/
+def fft(x):
+    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if np.log2(N) % 1 > 0:
+        raise ValueError("size of x must be a power of 2")
+
+    # N_min here is equivalent to the stopping condition above,
+    # and should be a power of 2
+    N_min = min(N, 32)
+
+    # Perform an O[N^2] DFT on all length-N_min sub-problems at once
+    n = np.arange(N_min)
+    k = n[:, None]
+    M = np.exp(-2j * np.pi * n * k / N_min)
+    X = np.dot(M, x.reshape((N_min, -1)))
+
+    # build-up each level of the recursive calculation all at once
+    while X.shape[0] < N:
+        X_even = X[:, :int(X.shape[1] / 2)]
+        X_odd = X[:, int(X.shape[1] / 2):]
+        factor = np.exp(-1j * np.pi * np.arange(X.shape[0])
+                        / X.shape[0])[:, None]
+        X = np.vstack([X_even + factor * X_odd,
+                       X_even - factor * X_odd])
+
+    return X.ravel()
 
 def stft(fft_size, data, pad_end_size, total_segments, hop_size, ):
     window = np.hanning(fft_size)  # our half cosine window
@@ -52,10 +81,11 @@ def stft(fft_size, data, pad_end_size, total_segments, hop_size, ):
         segment = proc[current_hop:current_hop + fft_size]  # get the current segment
         windowed = segment * window  # multiply by the half cosine function
         padded = np.append(windowed, inner_pad)  # add 0s to double the length of the data
-        spectrum = np.fft.fft(padded) / fft_size  # take the Fourier Transform and scale by the number of samples
+        spectrum = fft(padded) / fft_size  # take the Fourier Transform and scale by the number of samples
         autopower = np.abs(spectrum * np.conj(spectrum))  # find the autopower spectrum
         result[i, :] = autopower[:fft_size]  # append to the results array
     return result
+
 
 
 def convert_idx_to_time(time_list, idx):
@@ -87,8 +117,8 @@ def detect_hits(result):
     for i in range(1, len(averages)):
         if averages[i] > averages[i - 1] + loudness_offset:
             hits.append(i)
-    # plt.plot(averages, '.-')
-    # plt.show()
+    plt.plot(averages, '.-')
+    plt.show()
     return hits
 
 
@@ -151,10 +181,10 @@ for hit_idx in hits:
     key_and_times.append((find_key(pitch), convert_idx_to_time(time_list, hit_idx)))
 
 
-# results_transposed = np.transpose(results_cutoff)
-# img = plt.imshow(results_transposed, origin='lower', cmap='jet', interpolation='nearest', aspect='auto',
-#                  extent=[time_list[0], time_list[-1], freq_list[low_index_cutoff], freq_list[upper_index_cutoff]])
-# plt.show()
+results_transposed = np.transpose(results_cutoff)
+img = plt.imshow(results_transposed, origin='lower', cmap='jet', interpolation='nearest', aspect='auto',
+                 extent=[time_list[0], time_list[-1], freq_list[low_index_cutoff], freq_list[upper_index_cutoff]])
+plt.show()
 
 # !!! FINAL RESULT !!!
 
