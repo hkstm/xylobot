28023d835cbef8d6b54c6819409a983108b05977
Kailhan <l0venp34ce@gmail.com>
2020-01-21 11:03:49 +0100

    Update SignalArgParser.py

--- signalprocessing/SignalArgParser.py
+++ signalprocessing/SignalArgParser.py
@@ -6,7 +6,7 @@ def add_arguments(parser):
     parser.add_argument('-p', '--plot', action='store_true')
     parser.add_argument('-g', '--guiplot', action='store_true')
     parser.add_argument('-r', '--raw', action='store_true', help='Return data so it can be used for analysis')
-    parser.add_argument('-w', '--window', default='hanning', choices=['bartlett', 'blackman', 'hamming', 'hanning'],
+    parser.add_argument('-w', '--window', default='blackman', choices=['bartlett', 'blackman', 'hamming', 'hanning'],
                         help="Type of windowing function")
     parser.add_argument('-s', '--fftsize', default=512, type=int, help="FFT size")
     parser.add_argument('-l', '--level', nargs='?', default='error',
